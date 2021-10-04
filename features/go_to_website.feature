8fe59b8767c3d0928472b2608310f25248154409
Mike Bäck <omgmajk@users.noreply.github.com>
2021-10-04 13:56:36 +0200

    Added cookie clicker
    Cookie window now handled.

---
+++ features/go_to_website.feature
@@ -0,0 +1,6 @@
+Feature: Click accept cookie button
+
+    Scenario: Going to Willys and pressing the accept cookies button
+        Given that I am on "https://www.willys.se"
+        When I click the accept cookies button
+        #Then I should be able to use the website
---
+++ step-definitions/go_to_website.js
@@ -0,0 +1,13 @@
+module.exports = function() {
+  this.Given(/^that I am on "([^"]*)"$/, async function(url){
+    await helpers.loadPage(url);
+  });
+
+  this.When(/^I click the accept cookies button$/, async function(){
+    // Saving this code for later.
+    //await driver.wait(until.elementLocated(by.css('div.banner-actions-container')), 20000);
+    driver.manage().timeouts().implicitlyWait(20000);
+    let cookiesButtonSelect = await driver.findElement(by.id('onetrust-accept-btn-handler'));
+    await cookiesButtonSelect.click();
+  });
+}
