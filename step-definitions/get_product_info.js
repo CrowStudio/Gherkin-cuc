2c45b9882b1e07385db4b9800a00a87d6683d7b8
Lisa <lisahultgren@gmail.com>
2021-10-05 11:42:13 +0200

    Update get_product_info.js

--- step-definitions/get_product_info.js
+++ step-definitions/get_product_info.js
@@ -1,12 +1,12 @@
 module.exports = function () {
 
-  this.When(/^ I click on a product$/, async function () {
-    let imageButton = await driver.findElement(By.css('.ax.product-puff-image'));
+  this.When(/^I click on a product$/, async function () {
+    let imageButton = await driver.findElement(By.css('.resized-image'));
     await imageButton.click();
   });
 
   this.Then(/^I should get the clicked products information$/, async function () {
-    let productInfo = await driver.findElement(By.css('.md-dialog-content'));
+    let productInfo = await driver.findElement(By.css('.info-block.description'));
     await productInfo;
 
     //funkar ej
