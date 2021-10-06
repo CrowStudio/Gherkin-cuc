57d8a4345af2812bd7a5a4c4f0ca0d9b32adace3
Lisa <lisahultgren@gmail.com>
2021-10-06 14:23:35 +0200

    Update view_shopping _cart.js

--- step-definitions/view_shopping _cart.js
+++ step-definitions/view_shopping _cart.js
@@ -2,12 +2,17 @@ const { waitAWhile } = require('../helpers/wait.js');
 module.exports = function () {
 
   this.When(/^I click the shopping cart$/, async function () {
-    let shopCartButton = await driver.findElement(By.css('.mini-cart-desktop-btn'));
+    let shopCartButton = await driver.findElement(By.css('.mini-cart-desktop'));
     await shopCartButton.click();
 
   });
 
   this.Then(/^I should see the added products in my shopping cart$/, async function () {
+    let shopCart = await driver.findElement(By.css('.cart-mini-list'));
+    expect(shopCart).to.not.equal(0);
+    await waitAWhile(true);
+
+
 
   });
 
