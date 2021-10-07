8d156fba6f92fa258c1254ef111576766168f288
matildaorner <91562767+matildaorner@users.noreply.github.com>
2021-10-07 14:49:12 +0200

    Update update_shopping_cart

--- step-definitions/update_shopping_cart.js
+++ step-definitions/update_shopping_cart.js
@@ -1,25 +1,40 @@
 const { waitAWhile } = require('../helpers/wait.js');
 
-module.exports = function() {
+module.exports = function () {
+  
+  let quantityBeforeChange;
   
   this.When(/^I click the plus sign in the shopping cart$/, async function() {
+    quantityBeforeChange = +(await (await driver.findElement(By.css('input[name="quantity"]'))).getAttribute("value"));
     let plusButton = await driver.findElement(By.css('.ax-product-quantity-plus'));
     await plusButton.click();
     await waitAWhile(true);
   });
 
   this.When(/^I click the minus sign in the shopping cart$/, async function() {
+    quantityBeforeChange = +(await (await driver.findElement(By.css('input[name="quantity"]'))).getAttribute("value"));
     let plusButton = await driver.findElement(By.css('.ax-product-quantity-minus'));
     await plusButton.click();
     await waitAWhile(true);
   });
 
   this.When(/^I change the number directly in quantity to a random number$/, async function () {
+    quantityBeforeChange = +(await (await driver.findElement(By.css('input[name="quantity"]'))).getAttribute("value"));
     let quantity = Math.floor(Math.random() * (10 - 5 + 1) + 5);
     let quantityInput = await driver.findElement(By.css('input[name="quantity"]'));
     await quantityInput.sendKeys(selenium.Key.CONTROL + "a");
     await quantityInput.sendKeys(selenium.Key.DELETE);
     await quantityInput.sendKeys(quantity, (selenium.Key.ENTER));
+    await waitAWhile(true);
+  });
+
+  this.Then(/^the quantity should change$/, async function () {
+    let newQuantity = +(await (await driver.findElement(By.css('input[name="quantity"]'))).getAttribute("value"));
+    expect(quantityBeforeChange).to.not.equal(newQuantity);
+
+    //console.log('old number ' + quantityBeforeChange);
+    //console.log('new number ' + newQuantity);
+
     await waitAWhile(true);
   });
 
