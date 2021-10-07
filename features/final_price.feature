3f032616e82a31e63a97c0cd9469b8d92f270ca7
omgmajk <majkieboy@gmail.com>
2021-10-08 01:30:38 +0200

    Added some features
    Added some features-

--- features/final_price.feature
+++ features/final_price.feature
@@ -7,3 +7,8 @@ Feature: Check the total price
       And I click the accept cookies button
       And we have been through the initial select delivery popup
     Then I should be able to see some products
+
+  Scenario: Calculate the final price of ice cream
+    Given that I am on the Ice Cream category page
+    When I put one of each in the shopping cart
+    Then I should be able to calculate a final price
--- step-definitions/final_price.js
+++ step-definitions/final_price.js
@@ -1,5 +1,17 @@
 const { waitAWhile } = require('../helpers/wait.js');
 
+let productsToBuy;
+
 module.exports = function() {
-  
+  this.Given(/^that I am on the Ice Cream category page$/, async function(){
+    expect(0).to.equal(0);
+  });
+
+  this.When(/^I put one of each in the shopping cart$/, async function(){
+    expect(0).to.equal(0);
+  });
+
+  this.Then(/^I should be able to calculate a final price$/, async function(){
+    expect(0).to.equal(0);
+  });
 }
