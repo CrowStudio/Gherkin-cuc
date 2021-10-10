cdfd078f717f470f68bcda4dc1844d9525067d9f
omgmajk <majkieboy@gmail.com>
2021-10-10 23:36:20 +0200

    Update final_price.js
    Started step-definitions for final_price.js.

--- step-definitions/final_price.js
+++ step-definitions/final_price.js
@@ -4,7 +4,20 @@ let productsToBuy;
 
 module.exports = function() {
   this.Given(/^that I am on the Ice Cream category page$/, async function(){
-    expect(0).to.equal(0);
+    let frystLink = await driver.findElement(By.css('a[href="/sortiment/fryst"]'));
+    await frystLink.click();
+
+    await driver.wait(until.elementsLocated(By.css('a[href="/sortiment/fryst/glass"]')), 10000);
+    let glassLink = await driver.findElement(By.css('a[href="/sortiment/fryst/glass"]'));
+    await driver.executeScript('document.querySelector(\'a[href="/sortiment/fryst/glass"]\').scrollIntoView()');
+    await glassLink.click();
+
+    let headerText;
+    while (headerText !== 'Glass') {
+      headerText = await (await driver.findElement(By.css('h2'))).getText();
+      await driver.sleep(100);
+    }
+    expect(headerText).to.equal('Glass');
   });
 
   this.When(/^I put one of each in the shopping cart$/, async function(){
