f42302889f21380073b9c90c0e444301c5cb2d36
Lisa <lisahultgren@gmail.com>
2021-10-11 09:30:18 +0200

    added remove in package json

--- step-definitions/remove_product.js
+++ step-definitions/remove_product.js
@@ -2,4 +2,10 @@ const { waitAWhile } = require('../helpers/wait.js');
 
 module.exports = function () {
 
+  this.When(/^I click the minus button for a product in the shopping cart$/, async function () {
+    let minusButton = await driver.findElement(By.css(''));
+    await minusButton.click();
+
+  });
+
 }
