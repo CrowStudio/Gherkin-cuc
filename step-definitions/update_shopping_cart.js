11c260f10814271f68cf878cab92583e17468a92
CrowStudio <crowstudio@gmail.com>
2021-10-12 00:08:10 +0200

    Cleaned up code for better readabillity in update_shopping_cart.js

--- step-definitions/update_shopping_cart.js
+++ step-definitions/update_shopping_cart.js
@@ -5,23 +5,25 @@ module.exports = function () {
   let quantityBeforeChange;
   
   this.When(/^I click the plus sign in the shopping cart$/, async function() {
-    quantityBeforeChange = +(await (await driver.findElement(By.css('div[class^="product-quantity-sm selenium--mini-cart-quantity"]>div[class="ax-product-quantity-instock"]>form[class^="ax-product-quantity-input"]>input[name="quantity"]'))).getAttribute("value"));
-    let plusButton = await driver.findElement(By.css('div[class="product-quantity-sm selenium--mini-cart-quantity ax-product-quantity is-in-cart"]>div[class="ax-product-quantity-instock"]>button[class^="ax-btn ax-product-quantity-btn ax-product-quantity-plus"]'));
+    quantityBeforeChange = +(await (await driver.findElement(By.css('.md-3-line input[name="quantity"]'))).getAttribute("value"));
+    let plusButton = await driver.findElement(By.css('.md-3-line'));
+    plusButton = await plusButton.findElement(By.css('.selenium--product-quantity-add-to-cart-btn'));
     await plusButton.click();
     await waitAWhile(true);
   });
 
   this.When(/^I click the minus sign in the shopping cart$/, async function() {
-    quantityBeforeChange = +(await (await driver.findElement(By.css('div[class^="product-quantity-sm selenium--mini-cart-quantity"]>div[class="ax-product-quantity-instock"]>form[class^="ax-product-quantity-input"]>input[name="quantity"]'))).getAttribute("value"));
-    let minusButton = await driver.findElement(By.css('div[class="product-quantity-sm selenium--mini-cart-quantity ax-product-quantity is-in-cart"]>div[class="ax-product-quantity-instock"]>button[class^="ax-btn ax-product-quantity-btn ax-product-quantity-minus"]'));
+    quantityBeforeChange = +(await (await driver.findElement(By.css('.md-3-line input[name="quantity"]'))).getAttribute("value"));
+    let minusButton = await driver.findElement(By.css('.md-3-line'));
+    minusButton = await minusButton.findElement(By.css('.selenium--product-quantity-remove-from-cart-btn'));
     await minusButton.click();
     await waitAWhile(true);
   });
 
   this.When(/^I change the number directly in quantity to a random number$/, async function() {
-    quantityBeforeChange = +(await (await driver.findElement(By.css('div[class^="product-quantity-sm selenium--mini-cart-quantity"]>div[class="ax-product-quantity-instock"]>form[class^="ax-product-quantity-input"]>input[name="quantity"]'))).getAttribute("value"));
+    quantityBeforeChange = +(await (await driver.findElement(By.css('.md-3-line input[name="quantity"]'))).getAttribute("value"));
     let quantity = Math.floor(Math.random() * 6 + 5);
-    let quantityInput = await driver.findElement(By.css('div[class^="product-quantity-sm selenium--mini-cart-quantity"]>div[class="ax-product-quantity-instock"]>form[class^="ax-product-quantity-input"]>input[name="quantity"]'));
+    let quantityInput = await driver.findElement(By.css('.md-3-line input[name="quantity"]'));
     await quantityInput.sendKeys(selenium.Key.CONTROL + "a");
     await quantityInput.sendKeys(selenium.Key.DELETE);
     await quantityInput.sendKeys(quantity, selenium.Key.ENTER);
@@ -29,17 +31,17 @@ module.exports = function () {
   });
 
   this.Then(/^the quantity should change$/, async function() {
-    let newQuantity = +(await (await driver.findElement(By.css('div[class^="product-quantity-sm selenium--mini-cart-quantity"]>div[class="ax-product-quantity-instock"]>form[class^="ax-product-quantity-input"]>input[name="quantity"]'))).getAttribute("value"));
+    let newQuantity = +(await (await driver.findElement(By.css('.md-3-line input[name="quantity"]'))).getAttribute("value"));
     expect(quantityBeforeChange).to.not.equal(newQuantity);
     await waitAWhile(true);
   });
 
 
   this.When(/^I change the quantity of a product to a random letter$/, async function() {
-    quantityBeforeChange = +(await (await driver.findElement(By.css('div[class^="product-quantity-sm selenium--mini-cart-quantity"]>div[class="ax-product-quantity-instock"]>form[class^="ax-product-quantity-input"]>input[name="quantity"]'))).getAttribute("value"));
+    quantityBeforeChange = +(await (await driver.findElement(By.css('.md-3-line input[name="quantity"]'))).getAttribute("value"));
     let alphabet = "abcdefghijklmnopqrstuvwxyzåäö"
     let randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-    let quantityInput = await driver.findElement(By.css('div[class^="product-quantity-sm selenium--mini-cart-quantity"]>div[class="ax-product-quantity-instock"]>form[class^="ax-product-quantity-input"]>input[name="quantity"]'));
+    let quantityInput = await driver.findElement(By.css('.md-3-line input[name="quantity"]'));
     await quantityInput.sendKeys(selenium.Key.CONTROL + "a");
     await quantityInput.sendKeys(selenium.Key.DELETE);
     await quantityInput.sendKeys(randomLetter, selenium.Key.ENTER);
@@ -47,7 +49,7 @@ module.exports = function () {
   });
 
   this.Then(/^the quantity should remain the same as before$/, async function () {
-    let quantity = +(await (await driver.findElement(By.css('div[class^="product-quantity-sm selenium--mini-cart-quantity"]>div[class="ax-product-quantity-instock"]>form[class^="ax-product-quantity-input"]>input[name="quantity"]'))).getAttribute("value"));
+    let quantity = +(await (await driver.findElement(By.css('.md-3-line input[name="quantity"]'))).getAttribute("value"));
     await waitAWhile(true);
     expect(quantity).to.equal(quantityBeforeChange);
   });
